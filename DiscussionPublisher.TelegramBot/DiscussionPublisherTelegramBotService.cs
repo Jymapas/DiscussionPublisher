535a60550e5522a8116682a595aece6537c203fc
derevyankin.pg <pavelmurman@yandex.ru>
2023-04-18 14:28:47 +0300

    Upd

--- DiscussionPublisher.TelegramBot/DiscussionPublisherTelegramBotService.cs
+++ DiscussionPublisher.TelegramBot/DiscussionPublisherTelegramBotService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Extensions.Configuration;
-using Telegram.Bot.Types;
+﻿using Telegram.Bot.Types;
 using Telegram.Bot;
 
 namespace DiscussionPublisher.TelegramBot
@@ -10,12 +9,7 @@ namespace DiscussionPublisher.TelegramBot
 
         public void StartBot()
         {
-            var botConfig = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            _botClient = new TelegramBotClient(botConfig["TelegramBotSettings:ApiKey"]);
+            _botClient = new TelegramBotClient(Keys.BotKey);
         }
 
         public async Task SendMessageToChannelAsync(string channelName, string message)
@@ -25,7 +19,7 @@ namespace DiscussionPublisher.TelegramBot
 
         public async Task HandleUpdateAsync(Update update)
         {
-            //todo Реализуй здесь логику обработки сообщений от пользователей
+            //todo  Реализуй здесь логику обработки сообщений от пользователей
         }
     }
 }
