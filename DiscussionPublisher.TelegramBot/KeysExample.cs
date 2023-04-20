0541d2b7291816968a61264e9946996a0d6e2f83
derevyankin.pg <pavelmurman@yandex.ru>
2023-04-20 08:12:39 +0300

    Вот

---
+++ DiscussionPublisher.TelegramBot/KeysExample.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscussionPublisher.TelegramBot
+{
+    internal class Keys
+    {
+        internal static string BotKey = "enter_your_bot_key";
+        internal static long ChannelId = 0; // enter your channel id
+    }
+}
