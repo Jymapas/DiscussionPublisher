26e96c600561e30b30cfc7f66802c3d93903a726
derevyankin.pg <pavelmurman@yandex.ru>
2023-04-20 11:02:47 +0300

    upd

---
+++ DiscussionPublisherAPITests/Startup.cs
@@ -0,0 +1,59 @@
+﻿using DiscussionPublisher.TelegramBot;
+using Serilog;
+
+namespace DiscussionPublisherAPI
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSingleton<ITelegramBotService, DiscussionPublisherTelegramBotService>();
+
+            services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.ClearProviders();
+                loggingBuilder.AddSerilog(dispose: true);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            try
+            {
+                app.UseHttpsRedirection();
+                app.UseRouting();
+                app.UseErrorHandlingMiddleware();
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+                if (env.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.ApplicationServices.GetService<ITelegramBotService>()?.StartBot();
+            }
+            catch (Exception ex)
+            {
+                var logger = app.ApplicationServices.GetRequiredService<ILogger<Startup>>();
+                logger.LogError(ex, "An error occurred while configuring the application.");
+            }
+        }
+    }
+}
