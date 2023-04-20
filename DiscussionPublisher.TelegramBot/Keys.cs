b1d962088ebce1ab62b88a71e94b1c3d2406f9ab
derevyankin.pg <pavelmurman@yandex.ru>
2023-04-20 08:10:29 +0300

    Remove Keys.cs from git tracking

--- DiscussionPublisher.TelegramBot/Keys.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DiscussionPublisher.TelegramBot
-{
-    internal class Keys
-    {
-        internal static string BotKey = "enter_your_bot_key";
-        internal static long ChannelId = 0; // enter your channel id
-    }
-}
