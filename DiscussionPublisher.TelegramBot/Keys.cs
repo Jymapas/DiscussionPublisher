ec06b0dc2169c8983208ba0058587a558175ceea
derevyankin.pg <pavelmurman@yandex.ru>
2023-04-20 08:04:12 +0300

    test ignore

--- DiscussionPublisher.TelegramBot/Keys.cs
+++ DiscussionPublisher.TelegramBot/Keys.cs
@@ -3,6 +3,6 @@
     internal class Keys
     {
         internal static string BotKey = "enter_your_bot_key";
-        internal static long ChannelId = -0; // enter your channel id
+        internal static long ChannelId = 0; // enter your channel id
     }
 }
